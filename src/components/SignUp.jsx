73affd4f08d377f75c055ee021d3b0e83226637c
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-11-25 22:17:46 -0800

    Commiting changes

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
+import { firebaseApp } from "../firebase";
 
 class SignUp extends Component{
     constructor(props){
       super(props);
       this.state = {
         email: " ",
-        password: " "
+        password: " ",
+        error: {
+          message: " "
+        }
       }
+    }
+
+    signUp(){
+      console.log("this.state", this.state);
+
+      const { email, password } = this.state;
+      firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+      .catch(error => {
+        this.setState({error})
+      })
     }
 
   render(){
     return (
-      <div className="form-inline">
+      <div className="form-inline" style={{ margin: "5%" }}>
       <h2>Sign Up</h2>
       <div className="form-group">
       <input
@@ -29,10 +43,12 @@ class SignUp extends Component{
       <button
       className="btn btn-primary"
       type="button"
+      onClick={() => this.signUp()}
       >
       Sign Up
       </button>
       </div>
+      <div>{this.state.error.message}</div>
       </div>
     )
   }

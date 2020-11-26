161d2a0063952690696a97b66f1c1fbf851da9a6
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-11-25 21:53:30 -0800

    Commiting changes

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -3,8 +3,26 @@ import React, { Component } from "react";
 class SignUp extends Component{
   render(){
     return (
-      <div>
-      <h1>SignUp</h1>
+      <div className="form-inline">
+      <h2>Sign Up</h2>
+      <div className="form-group">
+      <input
+      className="form-control"
+      type="text"
+      placeholder="email"
+      />
+      <input
+      className="form-control"
+      type="password"
+      placeholder="password"
+      />
+      <button
+      className="btn btn-primary"
+      type="button"
+      >
+      Sign Up
+      </button>
+      </div>
       </div>
     )
   }

9d81d6a6b1f773cd599fd9ac8ae2b7987f117a09
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-11-26 23:51:21 -0800

    Commiting changes

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+import { SIGNED_IN } from "../constants";
+
+export function logUser() {
+  const action = {
+    type: SIGNED_IN,
+    email
+  }
+}
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const SIGNED_OUT = "SIGNED_OUT";
+export const SIGNED_IN = "SIGNED_IN";

24c57976fabfbaf9ecacfa48e8db63ca91a6e716
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-11-30 18:10:54 -0800

    Commiting changes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,11 @@
-import { combineReducers } from "redux";
-import user from "./reducer_user";
-import goals from "./reducer_goals";
-import completeGoals from "./reducer_completed_goals";
+// make a default state here for the user
+import { combineReducers } from 'redux';
+import user from './reducer_user';
+import goals from './reducer_goals';
+import completeGoals from './reducer_complete_goals';
 
 export default combineReducers({
-    user,
-    goals,
-    completeGoals
-});
+  user,
+  goals,
+  completeGoals
+})

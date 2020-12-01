05685fb39824c0eaf5b8a2181cf9a1b6c7dfa64d
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-11-30 18:17:14 -0800

    Commiting changes

--- src/index.js
+++ src/index.js
@@ -1,36 +1,80 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import { Router, Route, browserHistory } from "react-router";
-import { firebaseApp } from "./firebase";
-import { logUser } from "./actions";
-import reducer from "./reducers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { firebaseApp } from './firebase';
+import { logUser } from './actions';
+import reducer from './reducers';
 
-import App from "./components/App";
 import SignIn from './components/SignIn';
-import SignUp from "./components/SignUp";
+import SignUp from './components/SignUp';
+import App from './components/App';
 
 const store = createStore(reducer);
 
 firebaseApp.auth().onAuthStateChanged(user => {
-  if(user){
-  // console.log("user has signed in or up", user);
-  const { email } = user;
-  store.dispatch(logUser(email));
-  browserHistory.push("/app");
-}else {
-  // console.log("user has signed out or still needs to sign in.");
-  browserHistory.replace("/signin");
-}
+ if (user) {
+   let {email, uid} = user;
+   store.dispatch(logUser(email, uid)); // dispatches the logUser action to the reducers
+   browserHistory.push('/app');
+ } else {
+   // handling logouts
+   browserHistory.replace('/signin');
+ }
 })
 
 ReactDOM.render(
-  <Provider store={store}>
-  <Router path="/" history={browserHistory}>
-<Route path="/app" components={App} />
- <Route path="/signin" components={SignIn} />
- <Route path="/signup" components={SignUp} />
-  </Router>
-  </Provider>,
-  document.getElementById("root"));
+ <Provider store={store}>
+   <Router path="/" history={browserHistory}>
+     <Route path="/signin" component={SignIn}/>
+     <Route path="/signup" component={SignUp}/>
+     <Route path="/app" component={App}/>
+   </Router>
+ </Provider>,
+ document.getElementById('root')
+)
+
+
+
+
+
+
+
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { createStore } from "redux";
+// import { Provider } from "react-redux";
+// import { Router, Route, browserHistory } from "react-router";
+// import { firebaseApp } from "./firebase";
+// import { logUser } from "./actions";
+// import reducer from "./reducers"
+//
+// import App from "./components/App";
+// import SignIn from './components/SignIn';
+// import SignUp from "./components/SignUp";
+//
+// const store = createStore(reducer);
+//
+// firebaseApp.auth().onAuthStateChanged(user => {
+//   if(user){
+//   // console.log("user has signed in or up", user);
+//   const { email } = user;
+//   store.dispatch(logUser(email));
+//   browserHistory.push("/app");
+// }else {
+//   // console.log("user has signed out or still needs to sign in.");
+//   browserHistory.replace("/signin");
+// }
+// })
+//
+// ReactDOM.render(
+//   <Provider store={store}>
+//   <Router path="/" history={browserHistory}>
+// <Route path="/app" components={App} />
+//  <Route path="/signin" components={SignIn} />
+//  <Route path="/signup" components={SignUp} />
+//   </Router>
+//   </Provider>,
+//   document.getElementById("root"));

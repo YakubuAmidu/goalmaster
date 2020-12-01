fb7153a5b0d7560e62d10646659719bcb0b95d53
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-11-30 18:07:15 -0800

    Commiting changes

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,59 +1,133 @@
-import React, { Component } from "react";
-import { Link } from "react-router";
-import { firebaseApp } from "../firebase";
 
-class SignUp extends Component{
-    constructor(props){
-      super(props);
-      this.state = {
-        email: " ",
-        password: " ",
-        error: {
-          message: " "
-        }
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { firebaseApp } from '../firebase';
+
+class SignUp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      error: {
+        message: '' // test by filling this string with something like 'sign up error' to show off the error form
       }
     }
+  }
 
-    signUp(){
-      const { email, password } = this.state;
-      firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+  signUp() {
+    let { email, password } = this.state;
+    console.log('called signup', email, password)
+    firebaseApp.auth().createUserWithEmailAndPassword(email, password)
       .catch(error => {
-        this.setState({error})
+        console.log('error', error);
+        this.setState({error});
       })
-    }
+  }
 
-  render(){
+  render() {
     return (
-      <div className="form-inline" style={{ margin: "5%" }}>
-      <h2>Sign Up</h2>
-      <div className="form-group">
-      <input
-      className="form-control"
-      style={{ marginRight: "5px", marginBottom: "5px" }}
-      type="text"
-      placeholder="email"
-      onChange={event => this.setState({ email: event.target.value })}
-      />
-      <input
-      className="form-control"
-      style={{ marginRight: "5px", marginBottom: "5px" }}
-      type="password"
-      placeholder="password"
-      onChange={event => this.setState({ password: event.target.value })}
-      />
-      <button
-      className="btn btn-primary"
-      type="button"
-      onClick={() => this.signUp()}
-      >
-      Sign Up
-      </button>
-      </div>
-      <div>{this.state.error.message}</div>
-      <div><Link to="/signin">Already a user ? Sign in instead</Link></div>
+      <div style={{margin: '5%'}} className="form-inline">
+        <h2>Sign Up</h2>
+        <div className="form-group">
+          <input
+            className="form-control"
+            style={{marginRight: '5px'}}
+            type="text"
+            placeholder="email"
+            onChange={event => this.setState({email: event.target.value})}
+          />
+          <input
+            className="form-control"
+            style={{marginRight: '5px'}}
+            type="password"
+            placeholder="password"
+            onChange={event => this.setState({password: event.target.value})}
+          />
+          <button
+            onClick={() => this.signUp()}
+            className="btn btn-primary"
+            type="button"
+          >
+            Sign Up
+          </button>
+        </div>
+        {
+          <div>{this.state.error.message}</div>
+        }
+        <div>
+          <Link to={'/signin'}>Sign in instead</Link>
+        </div>
       </div>
     )
   }
 }
 
-export default SignUp;
+export default SignUp
+
+
+
+
+
+
+
+
+// import React, { Component } from "react";
+// import { Link } from "react-router";
+// import { firebaseApp } from "../firebase";
+//
+// class SignUp extends Component{
+//     constructor(props){
+//       super(props);
+//       this.state = {
+//         email: " ",
+//         password: " ",
+//         error: {
+//           message: " "
+//         }
+//       }
+//     }
+//
+//     signUp(){
+//       const { email, password } = this.state;
+//       firebaseApp.auth().createUserWithEmailAndPassword(email, password)
+//       .catch(error => {
+//         this.setState({error})
+//       })
+//     }
+//
+//   render(){
+//     return (
+//       <div className="form-inline" style={{ margin: "5%" }}>
+//       <h2>Sign Up</h2>
+//       <div className="form-group">
+//       <input
+//       className="form-control"
+//       style={{ marginRight: "5px", marginBottom: "5px" }}
+//       type="text"
+//       placeholder="email"
+//       onChange={event => this.setState({ email: event.target.value })}
+//       />
+//       <input
+//       className="form-control"
+//       style={{ marginRight: "5px", marginBottom: "5px" }}
+//       type="password"
+//       placeholder="password"
+//       onChange={event => this.setState({ password: event.target.value })}
+//       />
+//       <button
+//       className="btn btn-primary"
+//       type="button"
+//       onClick={() => this.signUp()}
+//       >
+//       Sign Up
+//       </button>
+//       </div>
+//       <div>{this.state.error.message}</div>
+//       <div><Link to="/signin">Already a user ? Sign in instead</Link></div>
+//       </div>
+//     )
+//   }
+// }
+//
+// export default SignUp;

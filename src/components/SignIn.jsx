c3b2f5600f07214c43be3895ddf3d1fa8400a2cf
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-11-30 18:04:55 -0800

    Commiting changes

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,59 +1,133 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { Link } from 'react-router';
-import { firebaseApp } from "../firebase";
+import { firebaseApp } from '../firebase';
 
-class SignIn extends Component{
-    constructor(props){
-      super(props);
-      this.state = {
-        email: " ",
-        password: " ",
-        error: {
-          message: " "
-        }
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { // explain that declaring a variable here becomes a class member
+      email: '',
+      password: '',
+      error: {
+        message: ''
       }
     }
+  }
 
-    signIn(){
-      const { email, password } = this.state;
-      firebaseApp.auth().signInWithEmailAndPassword(email, password)
+
+  signIn() {
+    let { email, password } = this.state;
+    firebaseApp.auth().signInWithEmailAndPassword(email, password)
       .catch(error => {
+        console.log('error', error)
         this.setState({error})
       })
-    }
+  }
 
-  render(){
+  render() {
     return (
-      <div className="form-inline" style={{ margin: "5%" }}>
-      <h2>Sign In</h2>
-      <div className="form-group">
-      <input
-      className="form-control"
-      style={{ marginRight: "5px", marginBottom: "5px" }}
-      type="text"
-      placeholder="email"
-      onChange={event => this.setState({ email: event.target.value })}
-      />
-      <input
-      className="form-control"
-      style={{ marginRight: "5px", marginBottom: "5px" }}
-      type="password"
-      placeholder="password"
-      onChange={event => this.setState({ password: event.target.value })}
-      />
-      <button
-      className="btn btn-primary"
-      type="button"
-      onClick={() => this.signIn()}
-      >
-      Sign In
-      </button>
-      </div>
-      <div>{this.state.error.message}</div>
-      <div><Link to="/signup">Sign up instead</Link></div>
+      <div style={{padding: '5%'}} className="form-inline">
+        <h2>Sign In</h2>
+        <div className="form-group">
+          <input
+            className="form-control"
+            style={{marginRight: '5px'}}
+            type="text"
+            placeholder="email"
+            onChange={event => this.setState({email: event.target.value})}
+          />
+          <input
+            className="form-control"
+            style={{marginRight: '5px'}}
+            type="password"
+            placeholder="password"
+            onChange={event => this.setState({password: event.target.value})}
+          />
+          <button
+            onClick={() => this.signIn()}
+            className="btn btn-primary"
+          >
+            Sign In
+          </button>
+        </div>
+        {
+          <div>{this.state.error.message}</div>
+        }
+        <div>
+          <Link to={'/signup'}>Sign up instead</Link>
+        </div>
       </div>
     )
   }
 }
 
 export default SignIn;
+
+
+
+
+
+
+
+
+
+// import React, { Component } from "react";
+// import { Link } from 'react-router';
+// import { firebaseApp } from "../firebase";
+//
+// class SignIn extends Component{
+//     constructor(props){
+//       super(props);
+//       this.state = {
+//         email: " ",
+//         password: " ",
+//         error: {
+//           message: " "
+//         }
+//       }
+//     }
+//
+//     signIn(){
+//       const { email, password } = this.state;
+//       firebaseApp.auth().signInWithEmailAndPassword(email, password)
+//       .catch(error => {
+//         this.setState({error})
+//       })
+//     }
+//
+//   render(){
+//     return (
+//       <div className="form-inline" style={{ margin: "5%" }}>
+//       <h2>Sign In</h2>
+//       <div className="form-group">
+//       <input
+//       className="form-control"
+//       style={{ marginRight: "5px", marginBottom: "5px" }}
+//       type="text"
+//       placeholder="email"
+//       onChange={event => this.setState({ email: event.target.value })}
+//       />
+//       <input
+//       className="form-control"
+//       style={{ marginRight: "5px", marginBottom: "5px" }}
+//       type="password"
+//       placeholder="password"
+//       onChange={event => this.setState({ password: event.target.value })}
+//       />
+//       <button
+//       className="btn btn-primary"
+//       type="button"
+//       onClick={() => this.signIn()}
+//       >
+//       Sign In
+//       </button>
+//       </div>
+//       <div>{this.state.error.message}</div>
+//       <div><Link to="/signup">Sign up instead</Link></div>
+//       </div>
+//     )
+//   }
+// }
+//
+// export default SignIn;

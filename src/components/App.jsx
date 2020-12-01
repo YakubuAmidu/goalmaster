4e5cbcc15c6a4bfe209e48c27d85b2a877151d0a
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-11-30 17:59:25 -0800

    Commiting changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,41 +1,79 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { firebaseApp } from "../firebase";
-import AddGoal from "./AddGoal";
-import GoalList from "./GoalList";
-import CompleteGoalList from "./CompleteGoalList";
 
-class App extends Component {
-    signOut(){
-      firebaseApp.auth().signOut();
-    }
 
-  render(){
-    return (
-      <div style={{ margin: "5px" }}>
-      <h3>Goals Master</h3>
+import React from 'react';
+import AddGoal from './AddGoal';
+import GoalList from './GoalList';
+import CompleteGoalList from './CompleteGoalList';
+import { firebaseApp } from '../firebase';
+
+function signOut() {
+  firebaseApp.auth().signOut();
+}
+
+const App = () => (
+    <div style={{padding: '5%'}}>
+      <h3>Goal Coach</h3>
       <AddGoal />
       <hr />
       <h4>Goals</h4>
       <GoalList />
       <hr />
-      <h4>Complete Goals</h4>
+      <h4>Completed Goals</h4>
       <CompleteGoalList />
       <hr />
-      <button
-      className="btn btn-danger"
-      onClick={() => this.signOut()}
-      >
-      Sign out
-      </button>
-      </div>
-    )
-  };
-};
+      <button onClick={() => signOut()} className="btn btn-danger">Sign Out</button>
+    </div>
+)
 
-function mapStateToProps(state){
-  // console.log("state", state);
-  return {}
-}
+export default App
 
-export default connect(mapStateToProps, null)(App);
+
+
+
+
+
+
+
+
+
+// import React, { Component } from "react";
+// import { connect } from "react-redux";
+// import { firebaseApp } from "../firebase";
+// import AddGoal from "./AddGoal";
+// import GoalList from "./GoalList";
+// import CompleteGoalList from "./CompleteGoalList";
+//
+// class App extends Component {
+//     signOut(){
+//       firebaseApp.auth().signOut();
+//     }
+//
+//   render(){
+//     return (
+//       <div style={{ margin: "5px" }}>
+//       <h3>Goals Master</h3>
+//       <AddGoal />
+//       <hr />
+//       <h4>Goals</h4>
+//       <GoalList />
+//       <hr />
+//       <h4>Complete Goals</h4>
+//       <CompleteGoalList />
+//       <hr />
+//       <button
+//       className="btn btn-danger"
+//       onClick={() => this.signOut()}
+//       >
+//       Sign out
+//       </button>
+//       </div>
+//     )
+//   };
+// };
+//
+// function mapStateToProps(state){
+//   // console.log("state", state);
+//   return {}
+// }
+//
+// export default connect(mapStateToProps, null)(App);
--- src/components/CompleteGoalList.jsx
+++ src/components/CompleteGoalList.jsx
@@ -1,56 +1,127 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { setCompleted } from "../actions";
-import { completeGoalRef } from "../firebase";
 
-class CompleteGoalList extends Component{
-  componentDidMount(){
-    completeGoalRef.on("value", snap => {
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setCompleted } from '../actions';
+import { completeGoalRef } from '../firebase';
+
+class CompleteGoalList extends Component {
+  componentDidMount() {
+    completeGoalRef.on('value', snap => {
       let completeGoals = [];
       snap.forEach(completeGoal => {
-        const { email, title } = completeGoal.val();
-        completeGoals.push({ email, title });
+        let {email, title} = completeGoal.val();
+        completeGoals.push({
+          email,
+          title
+        })
       })
+      // console.log('completeGoals', completeGoals)
       this.props.setCompleted(completeGoals);
-    })
+    });
   }
 
-  render(){
-    console.log("this.props.completeGoals", this.props.completeGoals);
+  clearCompleted() {
+    completeGoalRef.set([]);
+  }
 
-    return(
+  render() {
+    return (
       <div>
         {
-          this.completeGoals.map((completeGoal, index) => {
+          this.props.completeGoals.map((completeGoal, index) => {
             const { title, email } = completeGoal;
             return (
               <div key={index}>
-               <strong>{title}</strong> completed by <em>{email}</em>
+                <strong>{title}</strong> completed by <em>{email}</em>
               </div>
             )
           })
         }
-
-        clearCompleted(){
-          completeGoalRef.set([])
-        }
-
         <button
-        className="btn btn-primary"
-        onClick={() => this.clearCompleted()}
+          onClick={() => this.clearCompleted()}
+          className="btn btn-primary"
         >
-        Clear all
+          Clear All
         </button>
       </div>
     )
   }
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
   const { completeGoals } = state;
-  return{
+  return {
     completeGoals
   }
 }
 
-export default connect(mapStateToProps, { setCompleted })(CompleteGoalList);
+export default connect(mapStateToProps, { setCompleted })(CompleteGoalList)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from "react";
+// import { connect } from "react-redux";
+// import { setCompleted } from "../actions";
+// import { completeGoalRef } from "../firebase";
+//
+// class CompleteGoalList extends Component{
+//   componentDidMount(){
+//     completeGoalRef.on("value", snap => {
+//       let completeGoals = [];
+//       snap.forEach(completeGoal => {
+//         const { email, title } = completeGoal.val();
+//         completeGoals.push({ email, title });
+//       })
+//       this.props.setCompleted(completeGoals);
+//     })
+//   }
+//
+//   render(){
+//     console.log("this.props.completeGoals", this.props.completeGoals);
+//
+//     return(
+//       <div>
+//         {
+//           this.completeGoals.map((completeGoal, index) => {
+//             const { title, email } = completeGoal;
+//             return (
+//               <div key={index}>
+//                <strong>{title}</strong> completed by <em>{email}</em>
+//               </div>
+//             )
+//           })
+//         }
+//
+//         clearCompleted(){
+//           completeGoalRef.set([])
+//         }
+//
+//         <button
+//         className="btn btn-primary"
+//         onClick={() => this.clearCompleted()}
+//         >
+//         Clear all
+//         </button>
+//       </div>
+//     )
+//   }
+// }
+//
+// function mapStateToProps(state){
+//   const { completeGoals } = state;
+//   return{
+//     completeGoals
+//   }
+// }
+//
+// export default connect(mapStateToProps, { setCompleted })(CompleteGoalList);

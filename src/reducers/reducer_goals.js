631019d84f3345d090c198c7da2ce9ab2a237675
Yakubu Amidu <yakubuamidu@Yakubus-MacBook-Pro.local>
2020-11-30 18:12:23 -0800

    Commiting changes

--- src/reducers/reducer_goals.js
+++ src/reducers/reducer_goals.js
@@ -1,11 +1,27 @@
-import { SET_GOALS } from "../constants";
+import { SET_GOALS } from '../constants';
 
- export default (state = [], action) => {
-    switch(action.type){
-        case SET_GOALS:
-            const { goals } = action;
-             return goals;
-             default:
-                 return state;
-    }
+export default (state = [], action) => {
+  switch(action.type) {
+    case SET_GOALS:
+      const { goals } = action;
+      return goals;
+    default:
+      return state;
+  }
 }
+
+
+
+
+
+// import { SET_GOALS } from "../constants";
+//
+//  export default (state = [], action) => {
+//     switch(action.type){
+//         case SET_GOALS:
+//             const { goals } = action;
+//              return goals;
+//              default:
+//                  return state;
+//     }
+// }
--- src/reducers/reducer_user.js
+++ src/reducers/reducer_user.js
@@ -11,10 +11,34 @@ export default (state = user, action) => {
       user = {
         email
       }
-      return user;
+      return user
     default:
       return state
   }
 }
 
 
+
+
+
+
+
+
+// import { SIGNED_IN } from '../constants';
+//
+// let user = {
+//   email: null
+// }
+//
+// export default (state = user, action) => {
+//   switch (action.type) {
+//     case SIGNED_IN:
+//       const { email } = action;
+//       user = {
+//         email
+//       }
+//       return user;
+//     default:
+//       return state
+//   }
+// }
